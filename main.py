3f0c874a75d518cd10fd7b08652268bff48a1356
Ginchung-study-programming <ljcone@qq.com>
2018-06-12 17:34:52 +0800

    Modified file structure

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #coding:utf-8
+
+# Analyse the keyword of graduates of USTC, 2018. 
+# by Ginchung
+# A learner.
+# Updated on 2018.6.12
+
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud,ImageColorGenerator,STOPWORDS
+from wordcloud import WordCloud,ImageColorGenerator
 import jieba
-import numpy as np
 from scipy.misc import imread
 
 def genPic(req):
@@ -34,18 +39,18 @@ def genPic(req):
             string+=i[begin:end]
     
     
-    imgmask="bg-deepblue.bmp" # 或者 "bg-skyblue.bmp" 或其他你选用的图片作为轮廓背景(Mask)
+    imgmask=".\\backgrounds\\bg-deepblue.bmp" # 或者 "bg-skyblue.bmp" 或其他你选用的图片作为轮廓背景(Mask)
     seg=jieba.cut(string)
     seg_space=' '.join(seg)
     
-    alice_color=imread(imgmask)
+    pic_mask=imread(imgmask)
     
     #wordcloud默认不支持中文，这里的font_path需要指向中文字体，不然得到的词云全是乱码
-    wc=WordCloud(font_path='msyh.ttc',max_words=700,background_color='white',mask=alice_color,max_font_size=100,font_step=1).generate(seg_space)
-    imagecolor=ImageColorGenerator(alice_color)
+    wc=WordCloud(font_path='msyh.ttc',max_words=700,background_color='white',mask=pic_mask,max_font_size=100,font_step=1).generate(seg_space)
+    imagecolor=ImageColorGenerator(pic_mask)
     plt.imshow(wc.recolor(color_func=imagecolor))
     plt.axis("off")
-    wc.to_file(req+"-db"+".jpg")
+    wc.to_file(".\\Results\\"+req+"-db"+".jpg")
 
 def main():
     list=["1","2","3","4","5"]
